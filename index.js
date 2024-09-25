6a007a0e38d00d1fdb6a3fd945ca3d9c611f388d
Leonard <leonard.momber@posteo.de>
2024-09-25 14:25:57 +0200

    buttons and errror for api

--- index.js
+++ index.js
@@ -11,28 +11,58 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1; // changed to let
+let page = 1;
 const searchQuery = "";
 
-async function fetchCharacters() {
-  const response = await fetch(
-    "https://rickandmortyapi.com/api/character?page=<pageIndex>"
-  );
-  const data = await response.json();
-
-  return data.results;
+async function fetchCharacters(page) {
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}` // creation of templateString
+    );
+    if (!response.ok) {
+      throw new Error(`status:${response.status}`);
+    }
+    const data = await response.json();
+    maxPage = data.info.pages; //
+    console.log(data);
+    return data.results;
+  } catch (error) {
+    console.log("Error!!!");
+  }
 }
 
 async function renderCharacters() {
-  const characters = await fetchCharacters();
+  try {
+    const characters = await fetchCharacters(page);
 
-  cardContainer.innerHTML = "";
+    cardContainer.innerHTML = "";
 
-  characters.forEach((character) => {
-    const cardElement = CharacterCard(character);
-    cardContainer.append(cardElement);
-  });
+    characters.forEach((character) => {
+      const cardElement = CharacterCard(character);
+      cardContainer.append(cardElement);
+    });
+    // pagination between buttons
+    pagination.textContent = `seite ${page} von ${maxPage}`;
+  } catch (error) {
+    console.log("Wieder Error!!!");
+  }
 }
+
+//addevent button-next
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++; // each click adds +1
+    renderCharacters();
+  }
+});
+
+// addvent button-prev
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--; // clicks -1
+    renderCharacters();
+  }
+});
 
 renderCharacters();
